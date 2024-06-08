113de3f7cb81380a06cd5c38a729c561a37a77a4
Sumaib Hamza <89627208+SumaibH@users.noreply.github.com>
2024-06-08 14:26:36 +0500

    First

--- app/src/main/java/com/pro/resume/craft/fragments/splash/SplashFragment.java
+++ app/src/main/java/com/pro/resume/craft/fragments/splash/SplashFragment.java
@@ -6,29 +6,51 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.pro.resume.craft.R;
 import com.pro.resume.craft.databinding.FragmentSplashBinding;
 
 public class SplashFragment extends Fragment {
 
     private FragmentSplashBinding binding;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentSplashBinding.inflate(inflater, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        setEvents();
+    }
+
+    private void setEvents() {
+        NavController navController = Navigation.findNavController(requireView());
+
+        binding.continueToApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (firebaseAuth.getCurrentUser() != null) {
+                    // User is already logged in, navigate to the next screen
+                    navController.navigate(R.id.homeFragment);
+                } else {
+                    navController.navigate(R.id.loginFragment);
+                }
+            }
+        });
 
     }
 }
