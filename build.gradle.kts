cef1754201602bb5e3ea53899a8cd2d2f89cca2b
shifaalishauket <shifaalishauket@gmail.com>
2024-11-16 17:58:26 +0500

    Successfully Completed My Final Year Project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,14 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.0.2")
+        classpath("com.android.tools.build:gradle:8.1.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
         classpath("com.google.gms:google-services:4.4.1")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.2.1" apply false
+    id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("org.jetbrains.kotlin.kapt") version "1.8.0" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
